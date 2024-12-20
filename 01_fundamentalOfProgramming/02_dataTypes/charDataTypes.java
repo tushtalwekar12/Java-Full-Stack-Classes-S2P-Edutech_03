7890e15601d28c1e2d3f1a54b2ecacbcb79105ec
Tushar Talwekar <tushartalwekar08@gmail.com>
2024-12-20 19:30:02 +0530

    add new files

---
+++ 01_fundamentalOfProgramming/02_dataTypes/charDataTypes.java
@@ -0,0 +1,7 @@
+// char dataType 
+public class charDataTypes{
+    public static void main(String[] args) {
+        char ch = 'A'; // char is a single character
+        System.out.println(ch); // prints A
+    }
+}
